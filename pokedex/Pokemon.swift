372be208287244d5d5594689b04921ca13e21f59
Ryan Salim <ryansalimm@gmail.com>
2017-08-22 13:17:53 +0700

    creating pokemon class

--- pokedex/Pokemon.swift
+++ pokedex/Pokemon.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        if _name == nil {
+            _name = ""
+        }
+        return _name
+    }
+    
+    var pokedexId: Int {
+        if _pokedexId == nil {
+            _pokedexId = 0
+        }
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
