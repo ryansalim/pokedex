1196efe9d6853fd7fe323442f79e817bdbc1d991
Ryan Salim <ryansalimm@gmail.com>
2017-08-25 21:26:31 +0700

    Adding new VC and adding segue

---
+++ pokedex/PokemonDetailVC.swift
@@ -0,0 +1,22 @@
+//
+//  PokemonDetailVC.swift
+//  pokedex
+//
+//  Created by admin on 8/25/17.
+//  Copyright © 2017 Ryan Salim. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    var pokemon: Pokemon!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        nameLbl.text = pokemon.name
+    }
+
+}
